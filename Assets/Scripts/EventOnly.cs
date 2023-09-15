5238a94363c2588012688e2df479372c687f2f76
hoangviet-nguyen <viety.nguyen02@gmail.com>
2023-09-15 15:48:10 +0200

    Playing and testing with Interactions Template script for interaction implemented

---
+++ Assets/Scripts/EventOnly.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class EventOnly : Interactable
+{
+}
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public abstract class Interactable : MonoBehaviour
 {
+    [SerializeField] private bool useEvents;
+    private bool destroy;
     public string promptMessage;
 
     public void BasicInteract()
     {
+        if (useEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
 
@@ -15,6 +20,19 @@ public abstract class Interactable : MonoBehaviour
     {
         //This is a template function so here wont be any code
     }
-    
-   
+
+    public bool getEvents()
+    {
+        return useEvents;
+    }
+
+    public void setEvent(bool setting)
+    {
+        useEvents = setting;
+    }
+
+    public bool Destroy()
+    {
+        return destroy;
+    }
 }
