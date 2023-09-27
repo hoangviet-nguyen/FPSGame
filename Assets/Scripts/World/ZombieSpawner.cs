26e2c1f473afb5c9679c1ae5100e792b30b57904
viktor.weilenmann <viktor.weilenmann@students.fhnw.ch>
2023-09-27 08:38:22 +0200

    ZombieSpawner Updated, SpawnPoint Prefab created, Complete PLayer prefab Created

--- Assets/Scripts/World/ZombieSpawner.cs
+++ Assets/Scripts/World/ZombieSpawner.cs
@@ -1,54 +1,105 @@
-using Unity.AI.Navigation;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 namespace World
 {
     public class ZombieSpawner : MonoBehaviour
     {
-        [SerializeField] private GameObject zombiePrefab;
         [SerializeField] private Transform player;
+        [SerializeField] private GameObject zombiePrefab;
+        [SerializeField] private float maxDistance = 20f;
+        [SerializeField] private float spawnDelay = 3f;
+        [SerializeField] private int zombiesPerWave = 50;
+        [SerializeField] private int maxZombiesAlive = 20;
 
-        private NavMeshSurface navMeshSurface; // Reference to your NavMeshSurface component
+        private List<Vector3> spawnPoints = new List<Vector3>();
+        private List<GameObject> spawnedZombies = new List<GameObject>();
+        private int currentWave = 1;
+        private int zombiesRemainingInWave;
 
         void Start()
         {
-            navMeshSurface = FindObjectOfType<NavMeshSurface>(); // Assuming you have a NavMeshSurface component in your scene
-            
-            // Initiate 10 zombies in a circle around the point of x60 y15 z60 with a distance of 40 from the point
-            for (int i = 0; i < 10; i++)
-            {
-                float x = 60 + Mathf.Cos(i * Mathf.PI / 5) * 50;
-                float y = 15;
-                float z = 60 + Mathf.Sin(i * Mathf.PI / 5) * 50;
-                
-                Vector3 spawnPosition = new Vector3(x, y, z);
-                
-                // Sample the position to get the correct height on the NavMesh
-                NavMeshHit hit;
-                if (NavMesh.SamplePosition(spawnPosition, out hit, 10.0f, NavMesh.AllAreas))
-                {
-                    spawnPosition = hit.position;
-                }
-                else
-                {
-                    Debug.LogWarning("Failed to find a valid spawn position on the NavMesh.");
-                    continue; // Skip this iteration if no valid position was found
-                }
-                
-                GameObject zombie = Instantiate(zombiePrefab, spawnPosition, Quaternion.identity);
-                zombie.transform.parent = transform;
-                zombie.GetComponent<ZombieController>().target = player;
-            }
-
-            // After spawning the zombies, you may want to bake the NavMesh again to account for any changes.
-            //navMeshSurface.BuildNavMesh();
+            zombiePrefab.GetComponent<ZombieController>().target = player;
+            AddSpawnPoints();
+            zombiesRemainingInWave = zombiesPerWave;
+            StartCoroutine(SpawnZombiesWithDelay());
         }
 
-        // Update is called once per frame
-        void Update()
+        void AddSpawnPoints()
         {
-        
+            /*Verschiedene vordefinierte SpawnPoints auf der Map werden zur Liste hinzugefügt
+            spawnPoints.Add(new Vector3(68.8519974f, 5.20200014f, 106.237999f));
+            spawnPoints.Add(new Vector3(68.8519974f, 5.20200014f, 101.059998f));
+            spawnPoints.Add(new Vector3(68.4599991f, 5.57200003f, 92.0899963f));
+            spawnPoints.Add(new Vector3(83.9800034f, 4.57999992f, 106.040001f));
+            spawnPoints.Add(new Vector3(86.1800003f, 4.89099979f, 98.1999969f));
+            spawnPoints.Add(new Vector3(86.1800003f, 5.09000015f, 93.4899979f));
+            */
+            GameObject[] spawnPointObjects = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+            foreach (GameObject spawnPointObject in spawnPointObjects)
+            {
+                spawnPoints.Add(spawnPointObject.transform.position);
+            }
+        }
+
+        IEnumerator SpawnZombiesWithDelay()
+        {
+            while (true)
+            {
+                if (zombiesRemainingInWave <= 0)
+                {
+                    currentWave++;
+                    zombiesPerWave = currentWave + 5;
+                    zombiesRemainingInWave = zombiesPerWave;
+                }
+
+                if (spawnedZombies.Count < maxZombiesAlive)
+                {
+                    Vector3 closestSpawnPoint = Vector3.zero;
+                    float closestDistance = maxDistance;
+
+                    foreach (Vector3 spawnPoint in spawnPoints)
+                    {
+                        float distanceToPlayer = Vector3.Distance(player.position, spawnPoint);
+                        if (distanceToPlayer <= maxDistance && distanceToPlayer < closestDistance)
+                        {
+                            closestSpawnPoint = spawnPoint;
+                            closestDistance = distanceToPlayer;
+                        }
+                    }
+
+                    if (closestSpawnPoint != Vector3.zero && zombiesRemainingInWave > 0)
+                    {
+                        GameObject newZombie = SpawnZombie(closestSpawnPoint);
+                        zombiesRemainingInWave--;
+                    }
+                }
+
+                if (currentWave > 5)
+                {
+                    Debug.Log("All waves complete.");
+                    yield break;
+                }
+
+                yield return new WaitForSeconds(spawnDelay);
+            }
+        }
+
+        GameObject SpawnZombie(Vector3 spawnPosition)
+        {
+            GameObject zombie = Instantiate(zombiePrefab, spawnPosition, Quaternion.identity);
+            spawnedZombies.Add(zombie);
+            Debug.Log("Spawned a zombie at " + spawnPosition + "!");
+            return zombie;
+        }
+
+        void DespawnZombie(GameObject zombie)
+        {
+            //Sobald implementiert soll beim Tod eines Zombies diese Methode aufgerufen werden
+            spawnedZombies.Remove(zombie);
+            Destroy(zombie);
         }
     }
 }
