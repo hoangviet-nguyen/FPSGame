eb01be2664422bcf3ec4f371bb871fb799a35875
Marco Worni <marco.worni@students.fhnw.ch>
2023-10-03 23:37:47 +0200

    Zombie Death Animation added + 3 different for 3 zombies

--- Assets/Scripts/Zombie/ZombieController.cs
+++ Assets/Scripts/Zombie/ZombieController.cs
@@ -1,54 +1,65 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using World;
 
-public class ZombieController : MonoBehaviour {
-    private NavMeshAgent agent = null;
-    public Animator animator = null;
-    [SerializeField] PlayerHealth playerHealth;
-    [SerializeField] internal Transform target;
-    private ZombieStats stats;
-    private float _timeOfLastAttack = 0f;
+namespace Zombie {
+    public class ZombieController : MonoBehaviour {
+        private NavMeshAgent _agent;
+        private Animator animator;
+        [SerializeField] private PlayerHealth playerHealth;
+        [SerializeField] internal Transform target;
+        private ZombieStats _stats;
+        private float _timeOfLastAttack = 0f;
+        private static readonly int Speed = Animator.StringToHash("Speed");
+        private static readonly int Attack = Animator.StringToHash("Attack");
+        private ZombieSpawner _zombieSpawner;
+        private static readonly int Die1 = Animator.StringToHash("Die1");
 
-    private void Start() {
-        GetReferences();
-    }
+        private void Start() {
+            GetReferences();
+            _zombieSpawner = GameObject.FindGameObjectWithTag("ZombieSpawner").GetComponent<ZombieSpawner>();
+        }
 
-    private void Update() {
-        MoveToTarget();
-    }   
-    
-    private void MoveToTarget() {
-        agent.SetDestination(target.position);
-        animator.SetFloat("Speed", 1, 0.3f, Time.deltaTime);
-        RotateToTarget();
+        private void Update() {
+            _agent.SetDestination(target.position);
+            animator.SetFloat(Speed, 1, 0.3f, Time.deltaTime);
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            if (_stats.health <= 0){
+                _agent.isStopped = true;
+                animator.SetTrigger(Die1);
+                return;
+            }
+            
+            RotateToTarget();
         
-        float distance = Vector3.Distance(transform.position, target.position);
-            if (distance <= agent.stoppingDistance*1.2) {
-                animator.SetFloat("Speed", 0);
-                if (Time.time >= _timeOfLastAttack + stats.attackSpeed) {
+            if (distance <= _agent.stoppingDistance*1.2) {
+                animator.SetFloat(Speed, 0);
+                if (Time.time >= _timeOfLastAttack + _stats.attackSpeed) {
                     _timeOfLastAttack = Time.time;
-                    animator.SetTrigger("Attack");
-                    Debug.Log("Player hit");
-                    playerHealth.TakeDamage(stats.attackDamage);
+                    animator.SetTrigger(Attack);
+                    playerHealth.TakeDamage(_stats.attackDamage);
                 }
             }
+        }   
         
-    }
+        private void DespawnAfterAnim(GameObject zombie) {
+            if (zombie != null) {
+                _zombieSpawner.DespawnZombie(this.gameObject);
+            }
+        }
     
-    private void RotateToTarget() {
+        private void RotateToTarget() {
             Vector3 direction = (target.position - transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-
-    }
+        }
     
-    private void GetReferences() {
-        agent = GetComponent<NavMeshAgent>();
-        animator = GetComponentInChildren<Animator>();
-        stats = GetComponent<ZombieStats>();
-        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        private void GetReferences() {
+            _agent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+            _stats = GetComponent<ZombieStats>();
+            playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        }
     }
 }
--- Assets/Scripts/Zombie/ZombieStats.cs
+++ Assets/Scripts/Zombie/ZombieStats.cs
@@ -1,36 +1,22 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using World;
 
-public class ZombieStats : MonoBehaviour {
-    [SerializeField] private float health = 100;
-    [SerializeField] public float attackSpeed;
-    [SerializeField] public float attackDamage;
-    private ZombieSpawner zombieSpawner;
-    private float timeOfLastAttack = 0f;
+namespace Zombie {
+    public class ZombieStats : MonoBehaviour {
+        [SerializeField] public float health;
+        [SerializeField] public float attackSpeed;
+        [SerializeField] public float attackDamage;
 
-    private bool isDead = false;
-
-    private void Start()
-    {
-        zombieSpawner = GameObject.FindGameObjectWithTag("ZombieSpawner").GetComponent<ZombieSpawner>();
-    }
-
-    public void Update() {
-        if (health <= 0) {
-            Die();
+        public void Update() {
+            if (Input.GetKeyDown(KeyCode.Q)) {
+                TakeDamage(10);
+                Debug.Log("Zombie hit");
+            }
         }
-    }
 
-    public void TakeDamage(float damage) {
-        health -= damage;
-        Debug.Log(health);
-    }
-
-    private void Die() {
-        zombieSpawner.DespawnZombie(this.gameObject);
-        Debug.Log("Zombie died");
+        public void TakeDamage(float damage) {
+            health -= damage;
+            Debug.Log(health);
+        }
     }
 }
